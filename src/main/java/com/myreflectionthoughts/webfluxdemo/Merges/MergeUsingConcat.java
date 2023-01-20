b3bf75a8a74447b1cf8a889a80946ab04929ddb3
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-01-20 15:48:57 +0530

    * Added example using delayeed elements and test to verify the same.

--- src/main/java/com/myreflectionthoughts/webfluxdemo/Merges/MergeUsingConcat.java
+++ src/main/java/com/myreflectionthoughts/webfluxdemo/Merges/MergeUsingConcat.java
@@ -1,10 +1,19 @@
 package com.myreflectionthoughts.webfluxdemo.Merges;
 
+import java.time.Duration;
+import java.util.List;
+
 import reactor.core.publisher.Flux;
 
 /**
        * The operator concat() is used to merge two reactive streams.
        * This is a static method of the Flux class.
+       * Scenario:- Suppose there are two micro-services that return Flux<String> but have
+                   different responseTimes, this can cause disruption in the 
+                   expected sequence of the emitted Data.
+       * To overcome this we should use concat() operator.
+       * The Streams are subscribed one after the other, hence the sequence of emitted data is
+         preserved.
  */
 public class MergeUsingConcat {
  
@@ -19,5 +28,16 @@ public class MergeUsingConcat {
     */
        return Flux.concat(flux1,flux2);
     }  
+
+    public Flux<String> generateFlux_Concat(){
+         
+      Flux<String> flux1 = Flux
+                                .fromIterable(List.of(new String("flux Data 1"), new String("flux Data 2")))
+                                .delayElements(Duration.ofMillis(1000));
+      Flux<String> flux2 = Flux
+                                .fromIterable(List.of(new String("flux Data 3"), new String("flux Data 4")))
+                                .delayElements(Duration.ofMillis(1500));
+      return Flux.concat(flux1,flux2); 
+   }
 
 }
--- src/test/java/com/myreflectionthoughts/webfluxdemo/Merges/MergeUsingConcatTest.java
+++ src/test/java/com/myreflectionthoughts/webfluxdemo/Merges/MergeUsingConcatTest.java
@@ -22,5 +22,15 @@ public class MergeUsingConcatTest {
         StepVerifier.create(flux).expectNext("Hello 1", "Hello 2", "Hello 3","Hello 4").verifyComplete();
     }
 
+    @Test
+    void testGenerateFlux_WithConcat(){
+        /*
+               * The reactive streams are subscribed in a sequence
+                 hence they emit the data in a sequence.
+         */
+        Flux<String> combinedFlux = mergeUsingConcat.generateFlux_Concat();
+        StepVerifier.create(combinedFlux).expectNext("flux Data 1", "flux Data 2", "flux Data 3", "flux Data 4")
+                .verifyComplete();
+    }
      
 }
